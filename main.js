afb122a9f25f1f3291f3809174f1ec98032bd607
vithungf <tvhung260104@gmail.com>
2023-03-03 13:43:59 +0700

    update

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.__esModule = true;
-var productManager_1 = require("./controller/productManager");
-var adminManager_1 = require("./controller/adminManager");
 var MainMenu_1 = require("./view/MainMenu");
-var supplyManager = new productManager_1.ProductManager();
-var adminManager = new adminManager_1.AdminManager();
+// let productManager = new ProductManager();
+// let adminManager = new AdminManager();
 var login = new MainMenu_1.MainMenu();
 login.mainMenu();
+// let adminMenuSupplyManager = new AdminMenuProductManager()
+// adminMenuSupplyManager.AdminMenuProductManager()
--- main.ts
+++ main.ts
@@ -1,10 +1,15 @@
 import {ProductManager} from "./controller/productManager";
 import {AdminManager} from "./controller/adminManager";
 import {MainMenu} from "./view/MainMenu";
+import { AdminMenuProductManager } from "./view/AdminMenuProductManager";
 
 
-let supplyManager = new ProductManager();
-let adminManager = new AdminManager();
+// let productManager = new ProductManager();
+
+// let adminManager = new AdminManager();
 
 let login = new MainMenu();
 login.mainMenu()
+
+// let adminMenuSupplyManager = new AdminMenuProductManager()
+// adminMenuSupplyManager.AdminMenuProductManager()
